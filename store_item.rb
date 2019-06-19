b33017d661d66eddf8c9a1c84d62144b63a2b45c
meeae <meeaeking@gmail.com>
2019-06-18 19:01:59 -0500

    create a class to show data

--- store_item.rb
+++ store_item.rb
@@ -7,43 +7,79 @@
 # Bonus: Read more about Ruby’s hash symbol syntax:
 
 
-shirt = {
-  color: "blue",
-  price: 35,
-  inventory: 28
-}
+# shirt = {
+#   color: "blue",
+#   price: 35,
+#   inventory: 28
+# }
 
-pants = {
-  color: "yellow",
-  price: 50,
-  inventory: 30
-}
+# pants = {
+#   color: "yellow",
+#   price: 50,
+#   inventory: 30
+# }
 
-dress = {
-  color: "red",
-  price: 80,
-  inventory: 23
-}
+# dress = {
+#   color: "red",
+#   price: 80,
+#   inventory: 23
+# }
 
-p shirt[:color]
+# p shirt[:color]
 
-shirt = {
-  :color => "blue",
-  :price => 35,
-  :inventory => 28
-}
+# shirt = {
+#   :color => "blue",
+#   :price => 35,
+#   :inventory => 28
+# }
 
-pants = {
-  :color => "yellow",
-  :price => 50,
-  :inventory => 30
-}
+# pants = {
+#   :color => "yellow",
+#   :price => 50,
+#   :inventory => 30
+# }
 
-dress = {
-  :color => "red",
-  :price => 80,
-  :inventory => 23
-}
+# dress = {
+#   :color => "red",
+#   :price => 80,
+#   :inventory => 23
+# }
 
-p dress[:inventory]
+# p dress[:inventory]
 
+
+# Rewrite your store items using a class instead of a hash.
+# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
+# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
+
+class Storeitem
+  def initialize(item, color, price, inventory)
+    @item = item
+    @color = color
+    @price = price
+    @inventory = inventory
+  end
+  def item
+    @item
+  end
+
+  def color
+    @color
+  end
+
+  def price
+    @price
+  end
+  def inventory
+    @inventory
+  end
+
+  def color=(color)
+    @color = color
+  end
+end
+
+dress = Storeitem.new("dress", "blue", 36, 22)
+
+p dress.item 
+p dress.color = "yellow"
